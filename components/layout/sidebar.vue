11cf14be03ddc1cba134af2314eef6a06b3e8067
Xcution <xcution.js@gmail.com>
2024-08-03 23:52:11 +0700

    сделал меню сайдбара

--- components/layout/sidebar.vue
+++ components/layout/sidebar.vue
@@ -5,14 +5,13 @@
 <template>
     <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
         <NuxtLink to="/" class="mb-10 block">
-            <NuxtImg src="./logo.svg" alt="" width="100px"
+            <NuxtImg src="./logo.svg" alt="" width="140px"
             class="mx-auto"/>
         </NuxtLink>
         <button class="absolute top-2 right-3 transition-colors
         hover:text-primary">
             <Icon name="line-md:logout" size="24" />
         </button>
-
-        <UiButton>Send</UiButton>
+        <LayoutMenu />
     </aside>
 </template>
