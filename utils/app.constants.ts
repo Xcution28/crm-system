fc6534248de53311fb747719e5eb0151cbd06132
Xcution <xcution.js@gmail.com>
2024-08-04 13:27:58 +0700

    сделал страницу login

---
+++ utils/app.constants.ts
@@ -0,0 +1,6 @@
+export const APP_WRITE_ID = 'crm-system-nuxt-pinia'
+export const DB_ID = 'crm-system-base'
+export const COLLECTION_DEALS = 'deals'
+export const COLLECTION_CUSTOMERS = 'customers'
+export const COLLECTION_COMMENTS = 'comments'
+export const STORAGE_ID = 'storage'
