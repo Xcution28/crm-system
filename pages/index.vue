b81996f81179db9c229bb66d166c6b8f9909c678
Xcution <xcution.js@gmail.com>
2025-02-16 02:15:56 +0700

    добавил карточку и типы

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ICard, IColumn } from "../components/kanban/kanban.types"
+
+useSeoMeta({
+    title: 'Home | CRM System',
+})
+
+const dragCardRef = ref<ICard | null>(null)
+const sourceColumnRef = ref<IColumn | null>(null)
+</script>
 
 <template>
     <div class="p-10">
         <h1 class="font-bold text-2xl mb-10">CRM System</h1>
+        <div>
+            <UiCard class="mb-3" draggable="true">
+                <UiCardHeader role="button">name card</UiCardHeader>
+                <UiCardContent>Компания</UiCardContent>
+                <UiCardFooter>Date</UiCardFooter>
+            </UiCard>
+        </div>
     </div>
 </template>
