663d693c60fdfbe38c17d49bd0107d967aa13f04
Xcution <xcution.js@gmail.com>
2025-03-16 14:39:53 +0700

    добавил kanban-query

--- components/kanban/useKanbanQuery.ts
+++ components/kanban/useKanbanQuery.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/vue-query"
 import { KANBAN_DATA } from "./kanban.data"
-import { IDeal } from "~/types/deals.types"
+import { IDeal } from "../../types/deals.types"
 
 export function useKanbanQuery() {
 
@@ -11,6 +11,22 @@ export function useKanbanQuery() {
             const newBoard = [...KANBAN_DATA]
             const deals = data.documents as unknown as IDeal[]
             console.log(deals, newBoard)
+
+            deals.forEach((deal) => {
+                const column = newBoard.find(col => col.id === deal.status)
+                if (column) {
+                    column.items.push({
+                        $createdAt: deal.$createdAt,
+                        id: deal.$id,
+                        name: deal.name,
+                        price: deal.price,
+                        companyName: deal.customer.name,
+                        status: column.name
+                    })
+                }
+            })
+
+            return newBoard
         }
     })
 }
--- pages/index.vue
+++ pages/index.vue
@@ -8,13 +8,14 @@ useSeoMeta({
 
 // const dragCardRef = ref<ICard | null>(null)
 // const sourceColumnRef = ref<IColumn | null>(null)
-useKanbanQuery()
+const { data, isLoading, refetch } = useKanbanQuery()
 </script>
 
 <template>
     <div class="p-10">
         <h1 class="font-bold text-2xl mb-10">CRM System</h1>
-        <div>
+        <div v-if="isLoading">Loading...</div>
+        <div v-else>
             <UiCard class="mb-3" draggable="true">
                 <UiCardHeader role="button">name card</UiCardHeader>
                 <UiCardContent>Компания</UiCardContent>
