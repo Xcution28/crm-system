96f517eb8d2e8914216e252a6cd90f428e1c5508
Xcution28 <xcution.js@gmail.com>
2025-03-28 17:45:07 +0700

    добавил редактирование клиента

--- pages/customers/edit/[id].vue
+++ pages/customers/edit/[id].vue
@@ -1,9 +1,92 @@
-<script>
-export default {
-    name: "[id]"
+<script setup lang="ts">
+import type { ICustomer } from "~/types/deals.types"
+import {useMutation, useQuery} from "@tanstack/vue-query"
+import { DB } from "~/utils/appwrite"
+
+
+interface InputFileEvent extends Event {
+    target: HTMLInputElement
 }
+
+interface ICustomerFormState
+    extends Pick<ICustomer, 'avatar_url' | 'email' | 'name' | 'from_source'> {}
+
+useSeoMeta({
+    title: 'Редактирование компании'
+})
+
+const route = useRoute()
+const customerId = route.params.id as string
+
+const { handleSubmit, defineField, setFieldValue, setValues, values } = useForm<ICustomerFormState>()
+
+const { data, isSuccess } = useQuery({
+    queryKey: ['get customer', customerId],
+    queryFn: () => DB.getDocument(DB_ID, COLLECTION_CUSTOMERS, customerId)
+})
+
+watch(isSuccess, () => {
+    const initialData = data.value as unknown as ICustomerFormState
+    setValues({
+        email: initialData.email,
+        avatar_url: initialData.avatar_url,
+        from_source: initialData.from_source || '',
+        name: initialData.name
+    })
+})
+
+const [name, nameAttrs] = defineField('name')
+const [email, emailAttrs] = defineField('email')
+const [fromSource, fromSourceAttrs] = defineField('from_source')
+
+const { mutate, isPending } = useMutation({
+    mutationKey: ['update customer', customerId],
+    mutationFn: (data: ICustomerFormState) =>
+        DB.updateDocument(DB_ID, COLLECTION_CUSTOMERS, customerId, data)
+})
+
+const onSubmit = handleSubmit(values => {
+    mutate(values)
+})
 </script>
 
 <template>
-
+    <div class="p-10">
+        <h1 class="font-bold text-2xl mb-10">
+            Редактирование {{ (data as unknown as ICustomerFormState)?.name }}
+        </h1>
+        <form @submit="onSubmit" class="form">
+            <UiInput
+                placeholder="Наименование"
+                v-model="name"
+                v-bind="nameAttrs"
+                type="text"
+                class="input"
+            />
+            <UiInput
+                placeholder="Email"
+                v-model="email"
+                v-bind="emailAttrs"
+                type="text"
+                class="input"
+            />
+            <UiInput
+                placeholder="Откуда пришел?"
+                v-model="fromSource"
+                v-bind="fromSourceAttrs"
+                type="text"
+                class="input"
+            />
+            <UiButton :disabled="isPending" variant="secondary" class="mt-3">
+                {{ isPending ? 'Загрузка...' : 'Сохранить' }}
+            </UiButton>
+        </form>
+    </div>
 </template>
+
+<style scoped>
+.input {
+    @apply border-[#161c26] mb-2 placeholder:text-[#748092] focus:border-border
+    transition-colors;
+}
+</style>
